03ec59e6047250c3dca4b9995c9157ebb7a703ac
gabrieldeoliveiraestevam <gabrieldeoliveiraestevam@gmail.com>
2021-06-12 12:41:03 -0300

    visualiza_dados.py - inclui validação de arquivo base de dados

--- levantamento_dados/visualiza_dados.py
+++ levantamento_dados/visualiza_dados.py
@@ -1,6 +1,10 @@
 import json
 import sweetviz as sv
 import pandas as pd    
+import os
+
+def arquivo_json_existe(file_name):
+    return os.path.exists(file_name)
 
 def ler_arquivo_json(nome_arquivo):
     with open(nome_arquivo, 'r', encoding='utf8') as f:
@@ -25,14 +29,18 @@ def seleciona_informacoes_repositorio_json(arquivo):
 #================================================================================#
 nome_base_dados = "base_dados_repositorios.json"
 
-base_dados_json = ler_arquivo_json(nome_base_dados)
-base_dados = base_dados_json['items']
 
-lista = seleciona_informacoes_repositorio_json(base_dados)
+if arquivo_json_existe(nome_base_dados):
+    base_dados_json = ler_arquivo_json(nome_base_dados)
+    base_dados = base_dados_json['items']
 
-# Transforma o arquivo json em um data frame
-data_frame = pd.json_normalize(lista)
+    lista = seleciona_informacoes_repositorio_json(base_dados)
 
-analise_dados = sv.analyze(data_frame)
+    # Transforma o arquivo json em um data frame
+    data_frame = pd.json_normalize(lista)
 
-analise_dados.show_html('analise_dados.html')
+    analise_dados = sv.analyze(data_frame)
+
+    analise_dados.show_html('analise_dados.html')
+else:
+    print(f'Erro - Arquivo {str(nome_base_dados)} não foi localizados!')
