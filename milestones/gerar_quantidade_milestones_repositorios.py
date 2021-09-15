487c8f0bc88e69b4adeb2e516ea3793456c5c3cc
Gabriel de Oliveira Estevam <gabrieldeoliveiraestevam@gmail.com>
2021-09-14 21:34:36 -0300

    Inclui modulos de coleta de milestones

---
+++ milestones/gerar_quantidade_milestones_repositorios.py
@@ -0,0 +1,34 @@
+import json
+
+def ler_arquivo_json(nome_arquivo):
+    with open(nome_arquivo, 'r', encoding='utf8') as f:
+        return json.load(f)
+
+def gravar_arquivo_json(nome_arquivo, dados):
+    with open(nome_arquivo, 'w', encoding='utf-8') as f:
+        json.dump(dados, f, ensure_ascii=False, indent=2, sort_keys=False, separators=(',' , ':'))
+
+#================================================================================#
+# MAIN                                                                           #
+#================================================================================#
+
+print('Informe o arquivo.json dos repositórios com suas milestones: ')
+nome_arquivo = input()
+
+arquivo_json = ler_arquivo_json(nome_arquivo)
+
+arquivo_json_saida = []
+
+for x in range(len(arquivo_json)):
+    registro = {}
+    registro['id']                  = arquivo_json[x]['id'] 
+    registro['name']                = arquivo_json[x]['name']
+    registro['url']                 = arquivo_json[x]['url']
+    registro['created_at']          = arquivo_json[x]['created_at']
+    registro['num_watchers']        = arquivo_json[x]['num_watchers']   
+    registro['quantidade_milestones'] = len(arquivo_json[x]['milestones'])
+    arquivo_json_saida.append(registro)
+
+nome_arquivo_saida = f'quantidade-milestones-repositorio.json'
+
+gravar_arquivo_json(nome_arquivo_saida,arquivo_json_saida)
